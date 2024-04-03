d1374594ac30b30a2920bf67faedda2dd1196089
will <wlymes@gmail.com>
2024-04-03 09:07:54 -0300

    feat: port env add

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,21 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { env } from '@config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = parseInt(env.PORT || '5000', 10);
+
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards();
   app.enableCors({
     origin: '*',
   });
 
-  await app.listen(5000);
+  await app.listen(PORT);
+  Logger.log(`🚀 Application is running on: http://localhost:${PORT}`);
 }
 
 bootstrap();
