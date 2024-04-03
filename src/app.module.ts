75bf6657e732da6066443d75fa55f9270f7d3531
will <wlymes@gmail.com>
2024-04-03 09:36:11 -0300

    style: path import changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from '@modules/users/users.module';
+import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from '@database/database.module';
-import { AuthModule } from '@modules/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from '@modules/auth/auth.guard';
-import { SchedulesModule } from '@modules/schedules/schedules.module';
-import { ConnectionModule } from '@modules/connection/connection.module';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { SchedulesModule } from './modules/schedules/schedules.module';
+import { ConnectionModule } from './modules/connection/connection.module';
 
 @Module({
   imports: [
